68c406fb258f010c5ebbd82e57438e639f979ea5
Meera Balakrishnan <meera.balakrishnan@opn-mc-129.wwo.local>
2025-03-06 13:06:44 +0530

    initial commit

---
+++ src/app/headbar/headbar/headbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-headbar',
+  imports: [],
+  templateUrl: './headbar.component.html',
+  styleUrl: './headbar.component.css'
+})
+export class HeadbarComponent {
+
+}
